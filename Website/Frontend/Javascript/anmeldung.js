1a2890e7f3a6c84fdaed1f3d265c395903756d29
mto99 <ozmen.muhammed@outlook.com>
2021-06-17 19:52:58 +0200

    Registrierung: Fehler bei Passwortüberprüfung behoben.

--- Website/Frontend/Javascript/anmeldung.js
+++ Website/Frontend/Javascript/anmeldung.js
@@ -1,21 +1,15 @@
 //check if both passwords are equal
-function comparePasswords(){
-    var pw1 = document.getElementById("passwort").value;
-    var pw2 = document.getElementById("passwort_wdh").value;
-    console.log("PW: " +pw1 + " || " + pw2);
+function comparePasswords() {
+    var pw1 = document.getElementById('passwort1').value;
+    var pw2 = document.getElementById('passwort_wdh').value;
 
     if (pw1 != pw2){
         document.getElementById("alertPassword").innerHTML = "Passwörter stimmen nicht überein!";
         return false;
     }
 
-    else if (pw1 == pw2){
+    else {
         document.getElementById("alertPassword").innerHTML = "";
         return true;
     }
-
-    else{
-        throw new Error("[err] Passwords are not equal!");
-    }
 }
-
