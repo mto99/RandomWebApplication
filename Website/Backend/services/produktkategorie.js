ea98cc9d92c2048e232efc9b4745d595615f7671
mto99 <ozmen.muhammed@outlook.com>
2021-06-21 23:30:03 +0200

    Produktseiten: Verbessert und teilweise neu implementiert.

--- Website/Backend/services/produktkategorie.js
+++ Website/Backend/services/produktkategorie.js
@@ -5,6 +5,24 @@ var serviceRouter = express.Router();
 
 helper.log('- Service Produktkategorie');
 
+//===========================================================
+//Wurde von Muhammed hinzugefügt
+serviceRouter.get('/produktkategorie/gib/:category', function(request, response) {
+    helper.log('Service Produktkategorie: Client requested one record, id=' + request.params.id);
+
+    const produktkategorieDao = new ProduktkategorieDao(request.app.locals.dbConnection);
+    try {
+        var result = produktkategorieDao.loadCat(request.params.category);
+        helper.log('Service Produktkategorie: Record loaded');
+        response.status(200).json(helper.jsonMsgOK(result));
+    } catch (ex) {
+        helper.logError('Service Produktkategorie: Error loading record by id. Exception occured: ' + ex.message);
+        response.status(400).json(helper.jsonMsgError(ex.message));
+    }
+});
+
+//===========================================================
+
 serviceRouter.get('/produktkategorie/gib/:id', function(request, response) {
     helper.log('Service Produktkategorie: Client requested one record, id=' + request.params.id);
 
